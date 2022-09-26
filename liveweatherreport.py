79422b010992e7fe9ab66b1bb05ac66b2106fd12
SIPRA-BEJ <113777859+SIPRA-BEJ@users.noreply.github.com>
2022-09-26 15:27:01 +0530

    Update liveweatherreport.py

--- liveweatherreport.py
+++ liveweatherreport.py
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>
-LiveWeatherReport
-</h1>
-<p>
-LiveWeatherReport
-</p>
-
 from bs4 import BeautifulSoup
 import requests
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
@@ -31,6 +20,3 @@ city=input("Enter the Name of Any City >>  ")
 city=city+" weather"
 weather(city)
 
-
-</body>
-</html>
